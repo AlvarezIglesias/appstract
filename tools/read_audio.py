60cb1af4bea5f8d91ea2ea88f1cf7c83b9ce7e0d
Álvaro Corrochano López <42788636+Corrochano@users.noreply.github.com>
2024-03-29 21:23:24 +0100

    Audio to text test
    Added a script to test the API

--- tools/read_audio.py
+++ tools/read_audio.py
@@ -1,1 +1,32 @@
 import os
+
+import argparse
+
+from google.cloud import speech
+
+def transcribe_file(speech_file: str) -> speech.RecognizeResponse:
+    """Transcribe the given audio file."""
+    client = speech.SpeechClient()
+
+    with open(speech_file, "rb") as audio_file:
+        content = audio_file.read()
+
+    audio = speech.RecognitionAudio(content=content)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.MP3,
+        sample_rate_hertz=16000,
+        language_code="es",
+    )
+
+    response = client.recognize(config=config, audio=audio)
+
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(f"Transcript: {result.alternatives[0].transcript}")
+
+    return response
+
+transcribe_file(os.path.join("tmp", "mitocondria.mp3"))
+
