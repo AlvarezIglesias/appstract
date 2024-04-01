25a29d27e4b1cd0f532453b07d7690b8b972d21a
AlvarezIglesias <60479580+AlvarezIglesias@users.noreply.github.com>
2024-04-01 18:29:38 +0200

    Link post mp3 file to server
    Also added basic session

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask
-from flask import render_template
 import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+import random
+
 
 # Todo manage the tmp folder (delete the files an so on) 
 if not os.path.exists('tmp'):
@@ -8,6 +10,7 @@ if not os.path.exists('tmp'):
     os.makedirs('tmp')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = str(random.getrandbits(32))
 
 @app.route('/')
 def langing_page():
@@ -16,6 +19,42 @@ def langing_page():
 @app.route('/mainpage/')
 def main_page():
     return render_template("/main_page.html")
+
+
+# from flask documentation: https://flask.palletsprojects.com/en/2.3.x/patterns/fileuploads/
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+UPLOAD_FOLDER = '/tmp/'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           
+@app.route('/mainpage/file/', methods=['POST', 'GET'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
